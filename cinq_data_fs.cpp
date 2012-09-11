f5c4692ff15b5b2be6cbe32306a52dbb576c1712
Jinglei Ren <jinglei.ren@gmail.com>
2012-09-11 04:33:28 -0700

    Add cinqGetAttr() and underlying micro file metadata.

---
+++ cinq_data_fs.cpp
@@ -0,0 +1,52 @@
+#include <fuse.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+#include "cinquain_store.h"
+
+#include <string>
+#include <hash_map>
+#include <pthread.h>
+
+using namespace std;
+
+#define NUM_BUCKETS 1000
+
+// TODO Metadata
+static hash_map<string, struct stat> gKeyMeta(NUM_BUCKETS);
+static pthread_rwlock_t gMetaLock = PTHREAD_RWLOCK_INITIALIZER;
+
+static inline void SetKeyMeta(const char *ckey, const struct stat* meta) {
+  string skey(ckey);
+  pthread_rwlock_wrlock(&gMetaLock);
+  gKeyMeta[skey] = *meta;
+  pthread_rwlock_unlock(&gMetaLock);
+}
+
+static inline void GetKeyMeta(const char *ckey, struct stat* meta) {
+  string skey(ckey);
+  pthread_rwlock_rdlock(&gMetaLock);
+  *meta = gKeyMeta[skey];
+  pthread_rwlock_unlock(&gMetaLock);
+}
+
+static char *PathPick(consit char *path) {
+  const int len = strlen(path);
+  if (len == 0) return NULL;
+
+  char *last = path + len - 1;
+  while (last > path && *last != '/') {
+    --last;
+  }
+  
+  return *last == '/' ? last + 1 : last;
+}
+
+int cinqGetAttr(const char *path, struct stat *attr) {
+  char ckey = PathPick(path);
+  GetKeyMeta(path, attr);
+  return 0;
+}
+
+
+
